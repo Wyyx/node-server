b6aef545f170e021a5b0ee2ff685e020e08a5fad
Wyyx <gnehcyx@163.com>
2017-10-27 16:35:08 +0800

    Second commit

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
   fs.appendFile('server.log', log + '\n')
   next()
 })
+
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs', {
 //     pageTitle: 'Maintenance Page'
